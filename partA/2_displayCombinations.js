6b7a3b446fc66096acb3ce76a6fcec6f4b083d80
ShawnFrost26 <sb262202@gmail.com>
2024-01-31 22:41:48 +0530

    completed partA 2nd question

---
+++ partA/2_displayCombinations.js
@@ -0,0 +1,30 @@
+/*
+for the distribution,
+we need to calculate each of the sum of valueA and valueB.
+how many times the sum is occurring...
+example: sum of 7 can be obtained in multiple ways (1+6, 2+5, 3+4, 4+3, 5+2, 6+1) ==> 6 ways
+*/
+
+const dieA = [1, 2, 3, 4, 5, 6];
+const dieB = [1, 2, 3, 4, 5, 6];
+
+function calculateDistribution(dieA, dieB) {
+  let distribution = {};
+
+  for (let valueA of dieA) {
+    for (let valueB of dieB) {
+      const sum = valueA + valueB;
+
+      distribution[sum] = (distribution[sum] || 0) + 1;
+    }
+  }
+
+  return distribution;
+}
+
+const distribution = calculateDistribution(dieA, dieB);
+
+console.log("Distribution of Combinations:");
+for (let sum in distribution) {
+  console.log(`Sum ${sum}: ${distribution[sum]} occurrences`);
+}
