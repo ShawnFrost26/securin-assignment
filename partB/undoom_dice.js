d474a51b6f5eba04a698032fc89860621f5dbbdc
ShawnFrost26 <sb262202@gmail.com>
2024-02-01 09:11:21 +0530

    calculated original probabilities

--- partB/undoom_dice.js
+++ partB/undoom_dice.js
@@ -1,12 +1,25 @@
-function possibleSumProbability(dieA, dieB) {
-  let probabilities = {};
+//step 1 : calculate original probability distribution for the sums (2 to 12) based on the initial dice configuration.
 
-  for (let valueA of dieA) {
-    for (let valueB of dieB) {
-      const sum = valueA + valueB;
+function calculateOriginalProbabilities() {
+  const die_A = [1, 2, 3, 4, 5, 6];
+  const die_B = [1, 2, 3, 4, 5, 6];
+  const sumProbabilities = {};
 
-      probabilities[sum] =
-        (probabilities[sum] || 0) + 1 / (dieA.length * dieB.length);
+  for (let i = 0; i < die_A.length; i++) {
+    for (let j = 0; j < die_B.length; j++) {
+      const sum = die_A[i] + die_B[j];
+      sumProbabilities[sum] = (sumProbabilities[sum] || 0) + 1;
     }
   }
+
+  const totalCombinations = die_A.length * die_B.length;
+
+  for (const key in sumProbabilities) {
+    sumProbabilities[key] = sumProbabilities[key] / totalCombinations;
+  }
+
+  return sumProbabilities;
 }
+
+const originalProbabilities = calculateOriginalProbabilities();
+console.log("Original Probabilities:", originalProbabilities);
